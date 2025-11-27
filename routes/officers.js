1ef7a40c22c25d7063b76b29f1efa063d426ce13
Rohit Khatik <rohitkhatik1128@gmail.com>
2025-11-27 11:23:16 +0530

    fix bcrypt import in officers.js

--- routes/officers.js
+++ routes/officers.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const supabase = require('../supabaseClient');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
